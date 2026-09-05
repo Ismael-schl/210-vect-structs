ac039c6afb26149a9fc6e2000d0b793c3d90f285
Ismael-schl <ihadi678@insite.4cd.edu>
2026-09-05 16:26:19 -0700

    changed code to better reflect conventions of this course

--- 210-vect-struct/main.cpp
+++ 210-vect-struct/main.cpp
@@ -9,14 +9,29 @@ struct Color {
     int blue;
 };
 
+Color PopulateColor();
+void OutputColor(const Color &c);
+
 int main() {
-    Color myColor;
-    cout << "Enter your color's red hue value. then press enter: ";
-    cin >> myColor.red;
-    cout << "Enter your color's green hue value. then press enter: ";
-    cin >> myColor.green;
-    cout << "Enter your color's blue hue value. then press enter: ";
-    cin >> myColor.blue;
-    cout << "Red: " << myColor.red << " Green: " << myColor.green << " Blue: " << myColor.blue;
+    Color myColor = PopulateColor();
+    cout << "Confirming color values via output: " << endl;
+    OutputColor(myColor);
     return 0;
+}
+
+Color PopulateColor() {
+    Color temp;
+    cout << "Enter your color's red hue value. then press enter: ";
+    cin >> temp.red;
+    cout << "Enter your color's green hue value. then press enter: ";
+    cin >> temp.green;
+    cout << "Enter your color's blue hue value. then press enter: ";
+    cin >> temp.blue;
+    return temp;
+}
+
+void OutputColor(const Color &c) {
+    cout << "\t> Red hue value: " << c.red << endl;
+    cout << "\t> Green hue value: " << c.green << endl;
+    cout << "\t> Blue hue value: " << c.blue << endl;
 }
