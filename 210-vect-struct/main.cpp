c9fe849248be1dbdd363bf87ac6fa6f30eef8384
Ismael-schl <ihadi678@insite.4cd.edu>
2026-09-05 16:03:47 -0700

    1st 10 minutes

---
+++ 210-vect-struct/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct Color {
+    int red;
+    int green;
+    int blue;
+};
+
+int main() {
+    Color myColor;
+    cout << "Enter your color's red hue value. then press enter: ";
+    cin >> myColor.red;
+    cout << "Enter your color's green hue value. then press enter: ";
+    cin >> myColor.green;
+    cout << "Enter your color's blue hue value. then press enter: ";
+    cin >> myColor.blue;
+    cout << myColor.red << myColor.green << myColor.blue;
+    return 0;
+}
